99e368d4560a2c357d1b3c1494f3d8c1a318f185
jitto12 <jitto1011@gmail.com>
2025-04-15 17:29:08 +0530

    Modified the Input of the Program.

--- problems/src/arraypracticeproblems/MinSubArrayLen.java
+++ problems/src/arraypracticeproblems/MinSubArrayLen.java
@@ -2,27 +2,27 @@ package arraypracticeproblems;
 
 public class MinSubArrayLen {
     public static int minSubArrayLen(int target, int[] nums) {
-        int n = nums.length;
-        int minLength = Integer.MAX_VALUE;
-        int left = 0;
+        int i =0;
+        int min = Integer.MAX_VALUE;
         int sum = 0;
-
-        for (int right = 0; right < n; right++) {
-            sum += nums[right];
-
-            while (sum >= target) {
-                minLength = Math.min(minLength, right - left + 1);
-                sum -= nums[left];
-                left++;
+        for (int j = 0; j < nums.length; j++) {
+            sum = sum + nums[j];
+            while(sum >= target){
+                min = Math.min(min,j-i+1);
+                sum = sum - nums[i];
+                i++;
             }
         }
-
-        return (minLength == Integer.MAX_VALUE) ? 0 : minLength;
+        if (min == Integer.MAX_VALUE){
+            return 0;
+        }else{
+            return min;
+        }
     }
 
     public static void main(String[] args) {
-        int target = 7;
-        int[] nums = {2,3,1,2,4,3};
+        int target = 15;
+        int[] nums = {2,6,8,10,3,5,7};
         System.out.println(minSubArrayLen(target,nums));
     }
 }
